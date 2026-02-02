711456a3995198ee92230d7ea361a2dd85386cc0
Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
2026-02-02 16:35:18 +0000

    Added unit test project scaffolds

---
+++ tests/Unit/Core/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DfE.GetInformationAboutSchools.Prototyping.Core.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/Unit/Infrastructure/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DfE.GetInformationAboutSchools.Prototyping.Infra.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
