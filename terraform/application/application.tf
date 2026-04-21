463b58129d8b2e2df78982b6fa0bc33b9129b4ac
Spencer O'Hegarty <49732575+spanersoraferty@users.noreply.github.com>
2026-04-21 17:28:47 +0100

    Add validation patterns for address fields

--- terraform/application/application.tf
+++ terraform/application/application.tf
@@ -15,12 +15,12 @@ module "application_configuration" {
   # -----------------------------
   config_variables = {
     # Logging
-    Logging__LogLevel__Default               = "Information"
-    Logging__LogLevel__Microsoft.AspNetCore  = "Warning"
-  
+    Logging__LogLevel__Default              = "Information"
+    Logging__LogLevel__Microsoft.AspNetCore = "Warning"
+
     # Allowed hosts
     AllowedHosts = "*"
-  
+
     # CsvMappings - Establishment
     CsvMappings__Establishment__Columns__0 = "Identifier.Urn"
     CsvMappings__Establishment__Columns__1 = "BasicDetails.Name"
@@ -30,7 +30,7 @@ module "application_configuration" {
     CsvMappings__Establishment__Columns__5 = "Address.Street"
     CsvMappings__Establishment__Columns__6 = "Address.Town"
     CsvMappings__Establishment__Columns__7 = "Address.Postcode"
-  
+
     CsvMappings__Establishment__Headers__0 = "URN"
     CsvMappings__Establishment__Headers__1 = "EstablishmentName"
     CsvMappings__Establishment__Headers__2 = "EstablishmentType"
@@ -39,20 +39,20 @@ module "application_configuration" {
     CsvMappings__Establishment__Headers__5 = "Address_Street"
     CsvMappings__Establishment__Headers__6 = "Address_Town"
     CsvMappings__Establishment__Headers__7 = "Address_Postcode"
-  
+
     # CsvMappings - EstablishmentGroup
     CsvMappings__EstablishmentGroup__Columns__0 = "Identifier.UID"
     CsvMappings__EstablishmentGroup__Columns__1 = "BasicDetails.Name"
     CsvMappings__EstablishmentGroup__Columns__2 = "BasicDetails.GroupType"
     CsvMappings__EstablishmentGroup__Columns__3 = "GroupEstablishments[].URN"
     CsvMappings__EstablishmentGroup__Columns__4 = "GroupEstablishments[].Name"
-  
+
     CsvMappings__EstablishmentGroup__Headers__0 = "UID"
     CsvMappings__EstablishmentGroup__Headers__1 = "GroupName"
     CsvMappings__EstablishmentGroup__Headers__2 = "GroupType"
     CsvMappings__EstablishmentGroup__Headers__3 = "URN"
     CsvMappings__EstablishmentGroup__Headers__4 = "EstablishmentName"
-  
+
     # DefaultRequiredFields - Establishments
     DefaultRequiredFields__RequiredFields__Establishments__0 = "URN"
     DefaultRequiredFields__RequiredFields__Establishments__1 = "EstablishmentName"
@@ -64,13 +64,20 @@ module "application_configuration" {
     DefaultRequiredFields__RequiredFields__Establishments__7 = "Town"
     DefaultRequiredFields__RequiredFields__Establishments__8 = "Postcode"
     DefaultRequiredFields__RequiredFields__Establishments__9 = "EstablishmentStatus"
-  
+
     # DefaultRequiredFields - EstablishmentGroups
     DefaultRequiredFields__RequiredFields__EstablishmentGroups__0 = "UID"
     DefaultRequiredFields__RequiredFields__EstablishmentGroups__1 = "GroupName"
     DefaultRequiredFields__RequiredFields__EstablishmentGroups__2 = "GroupTypeName"
     DefaultRequiredFields__RequiredFields__EstablishmentGroups__3 = "EstablishmentURN"
     DefaultRequiredFields__RequiredFields__EstablishmentGroups__4 = "EstablishmentName"
+
+    # ValidationPatterns
+    ValidationPatterns__Patterns__Street    = "^(UNDEFINED|[\\p{L}\\p{N}'(\\)][\\p{L}\\p{N}\\s\\.,'\\/\\-\\–—\\&\\(\\):]{1,99})$"
+    ValidationPatterns__Patterns__Town      = "^(UNDEFINED|[\\p{L}\\p{N}][\\p{L}\\p{N}\\s\\.,'\\/\\-\\(\\)\\&]{0,49})$"
+    ValidationPatterns__Patterns__Postcode  = "^(UNDEFINED|(GIR 0AA|[A-Za-z]{1,2}[0-9][0-9A-Za-z]?\\s?[0-9][A-Za-z]{2}|[A-Za-z0-9][A-Za-z0-9\\s\\-\\/]{1,11}))$"
+    ValidationPatterns__Patterns__Website   = "^(UNDEFINED|(?:https?:\\/\\/|www\\.)?[A-Za-z0-9.-]+\\.[A-Za-z]{2,}(?:\\/\\S*)?)$"
+    ValidationPatterns__Patterns__Telephone = "^(UNDEFINED|(?:\\d{7,14}|0\\d{6,13}|44\\d{5,12}))$"
   }
 
   # -----------------------------
